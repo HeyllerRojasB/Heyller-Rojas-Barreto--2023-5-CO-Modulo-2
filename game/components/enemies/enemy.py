a3fd268bb2acc11f4860e6e020db38ec20d166bb
HeyllerRojasB <136202499+HeyllerRojasB@users.noreply.github.com>
2023-06-14 23:46:04 -0500

    Third-Class

--- game/components/enemies/enemy.py
+++ game/components/enemies/enemy.py
@@ -2,6 +2,7 @@ import pygame
 import random
 
 from pygame.sprite import Sprite
+from game.components.bullets.bullet import Bullet
 from game.utils.constants import ENEMY_1, ENEMY_2, SCREEN_HEIGHT, SCREEN_WIDTH
 
 
@@ -25,11 +26,12 @@ class Enemy(Sprite):
         self.movement_x = self.MOV_X[random.randint(0, 1)]
         self.move_x_for = random.randint(30, 100)
         self.index = 0
+        self.type = 'enemy'
+        self.shooting_time = random.randint(30, 50)
         
-        
-        
-    def update(self, ships):
-        self.rect.y += self.SPEED_Y
+    def update(self, ships, game):
+        self.rect.y += self.speed_y
+        self.shoot(game.bullet_manager)
         
         if self.movement_x == 'left':
             self.rect.x -= self.speed_x
@@ -62,3 +64,10 @@ class Enemy(Sprite):
             self.enemy_type = ENEMY_1
             self.speed_x = self.SPEED_X
         self.image = pygame.transform.scale(self.enemy_type, (self.ENEMY_WIDTH, self.ENEMY_HIGHT))
+
+    def shoot(self, bullet_manager):
+        current_time = pygame.time.get_ticks()
+        if self.shooting_time <= current_time:
+            bullet = Bullet(self)
+            bullet_manager.add_bullet(bullet)
+            self.shooting_time += random.randint(30, 50)
